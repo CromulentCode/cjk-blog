a89e5cc996a71eaa66b926ecac8edd495505983f
CromulentCode <116487366+CromulentCode@users.noreply.github.com>
2024-12-28 16:34:06 +0100

    Change font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -7,8 +7,8 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		fontFamily: {
-			sans: ['"LXGW WenKai GB Screen"', ...defaultTheme.fontFamily.sans],
-			serif: ['"LXGW WenKai GB Screen"', ...defaultTheme.fontFamily.serif],
+			sans: ['"meiryob"', ...defaultTheme.fontFamily.sans],
+			serif: ['"chihayajun"', ...defaultTheme.fontFamily.serif],
 			mono: ['Menlo', 'Consolas', ...defaultTheme.fontFamily.mono],
 			'default-sans': defaultTheme.fontFamily.sans,
 			'default-serif': defaultTheme.fontFamily.serif,

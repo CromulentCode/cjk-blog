d54483489508bdcf2d3e2f772f8a234186168e79
CromulentCode <116487366+CromulentCode@users.noreply.github.com>
2024-12-28 17:24:18 +0100

    Fix site settings causing ver cel errors

--- src/config.ts
+++ src/config.ts
@@ -11,27 +11,7 @@ export const PageSize = 15;
 socialPlatform => userName
 // check components/Header.astro socialConfig for more info
 export const Socials: Record<string, Record<string, string>> = {
-//	mail: { url: 'mailto:i@ladit.me' },
-//	github: { url: 'https://github.com/ladit/astro-blog-zozo' },
-//	x: { url: 'https://x.com/userName' },
-//	mastodon: { url: 'https://mastodon.social/@userName' },
-//	facebook: { url: 'https://facebook.com/userName' },
-//	instagram: { url: 'https://instagram.com/userName' },
-//	telegram: { url: 'https://t.me/@userName' },
-//	youtube: { url: 'https://youtube.com/@userName' },
-//	skype: { url: 'https://skype.com/userName' },
-//	slack: { url: 'https://slack.com/userName' },
-//	messenger: { url: 'https://messenger.com/userName' },
-//	whatsapp: { url: 'https://whatsapp.com/userName' },
-//	snapchat: { url: 'https://snapchat.com/userName' },
-//	line: { url: 'https://line.com/userName' },
-//	twitch: { url: 'https://twitch.com/userName' },
-//	weibo: { url: 'https://weibo.com/userName' },
-//	bilibili: { url: 'https://space.bilibili.com/userName' },
-//	dingding: { url: 'https://dingtalk.com/userName' },
-//	zhihu: { url: 'https://zhihu.com/userName' },
-//	douban: { url: 'https://douban.com/userName' },
-//	discord: { url: '/rss.xml' },
+    discord: { url: 'https://discordapp.com/users/999363475111415840' },
 };
 
 // doc: https://giscus.app
